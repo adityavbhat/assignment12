5d159a00692d0f7db649c4641e69af64ed31954d
adityavbhat <39955849+adityavbhat@users.noreply.github.com>
2018-06-20 19:51:59 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,10 @@
+# Definition of access token :-
+# Access tokens are the thing that applications use to make API requests on behalf of a user.
+# The access token represents the authorization of a specific application to access specific parts of a user’s data.
+
+""" Facebook Access Token
+     "id": "978037102242380"
+     "name": "Aditya Bhat"
+"""
+
+  
---
+++ q4.py
@@ -0,0 +1,7 @@
+"""A library is a collection of functions / objects that serves one particular purpose.
+   You could use a library in a variety of projects. (Various specialized services in our case)
+
+   An API is an interface for other programs to interact with your program or library  without having direct access.
+   ( giving specifications for our need to various vendors in our case)
+"""
+""" Ex:- Angular js- a JS framework may use many libraries like iniline editing of text using an exposed API of that library."""
